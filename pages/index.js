31763a1c176ccdd8d2a2146ad71510d886503a4f
Kai Wood <kwood@kwd.io>
2018-08-26 18:09:45 +0200

    Unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Header, Feed, Icon } from "semantic-ui-react";
-import { Stream } from "../ingredients/stream";
-import pnut from "pnut-butter";
+import { Header } from "semantic-ui-react";
 
 export default class Index extends Component {
   render() {
