684612099b90c6a0d34e8e37db0328c7ce33927f
Kai Wood <kwood@kwd.io>
2018-09-28 09:10:11 +0200

    Import userstream socket

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,16 @@ import { Layout } from "../ingredients/layout";
 import Router from "next/router";
 
 export default class JellyTime extends App {
+  state = {
+    socket: null
+  };
   componentDidMount() {
     const token = sessionStorage.getItem("token");
     if (!token && window.location.pathname !== "/") {
       window.location = "/sync-auth";
     }
+
+    this.connectToUserStream(token);
   }
 
   render() {
@@ -16,7 +21,7 @@ export default class JellyTime extends App {
     return (
       <Container>
         <Layout navigation={this.navigation}>
-          <Component {...pageProps} />
+          <Component {...pageProps} socket={this.state.socket} />
         </Layout>
       </Container>
     );
@@ -24,5 +29,40 @@ export default class JellyTime extends App {
 
   navigation = page => {
     Router.push(page);
+  };
+
+  connectToUserStream = async token => {
+    const ws = new WebSocket(
+      `wss://stream.pnut.io/v0/user?access_token=${token}`
+    );
+
+    let pingInterval = setInterval(() => {
+      console.log("ping");
+      ws.send("ping");
+    }, 45000);
+
+    ws.addEventListener("open", event => {
+      console.log("Socket connection established");
+      console.log(event.data);
+    });
+
+    ws.addEventListener("message", event => {
+      let { meta, data } = JSON.parse(event.data);
+
+      console.log(meta, data);
+
+      if (meta.connection_id) {
+        sessionStorage.setItem("connection_id", meta.connection_id);
+      }
+    });
+
+    ws.addEventListener("close", event => {
+      console.log("Socket closed");
+      console.log(event);
+
+      clearInterval(pingInterval);
+    });
+
+    this.setState({ socket: ws });
   };
 }
